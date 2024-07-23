cb8bc24ac6c0cccc7f462357436b927713a0b3e4
v5run <varun11pathak@gmail.com>
2024-07-23 15:15:06 -0400

    added the first mining capabilities w/ L7 requirement through Sha256

--- src/main/java/garden/Block.java
+++ src/main/java/garden/Block.java
@@ -8,6 +8,7 @@ public class Block {
     public String prevHash;
     private String data;
     private Long time;
+    private int nonce; // the "special number" needed to crack the code
 
     public Block(String data, String prevHash){
         this.data = data;
@@ -17,7 +18,7 @@ public class Block {
     }
 
     public String calcHash(){
-        String digest = prevHash + Long.toString(time) + data;
+        String digest = prevHash + Long.toString(time) + Integer.toString(nonce) + data;
         String newHash = StringUtil.sha256(digest); 
         return newHash;
     }
@@ -31,5 +32,15 @@ public class Block {
                 ", timeStamp=" + time +
                 '}' + "\n";
     }
+
+    public void mineBlock(int difficulty) {
+        this.nonce = 0;
+		String target = new String(new char[difficulty]).replace('\0', '0');
+		while(!hash.substring(0, difficulty).equals(target)) {
+			this.nonce ++;
+			hash = calcHash();
+		}
+		System.out.println("Block Mined! : " + hash + "\nN-once: " + Integer.toString(nonce));
+	}
 
 }
