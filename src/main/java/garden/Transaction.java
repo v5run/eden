1a5232fb72c2bd9114e3936e2909aa311e72bfaf
v5run <varun11pathak@gmail.com>
2024-08-15 16:54:58 -0400

    missing import for curve gen

---
+++ src/main/java/garden/Transaction.java
@@ -0,0 +1,7 @@
+package garden;
+import java.security.*;
+import java.util.ArrayList;
+
+public class Transaction {
+    
+}
--- src/main/java/garden/Wallet.java
+++ src/main/java/garden/Wallet.java
@@ -1,5 +1,11 @@
 package garden;
-import java.security.*;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.SecureRandom;
+import java.security.spec.ECGenParameterSpec;
+
 
 public class Wallet {
     public PrivateKey privateKey;
