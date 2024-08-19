95ee088c1e1a13ce0e8fb83178366e12624c7d0f
v5run <varun11pathak@gmail.com>
2024-08-19 01:41:11 -0400

    feasible transactions to others via public key + balance checking + utxo implementations

--- src/main/java/garden/Transaction.java
+++ src/main/java/garden/Transaction.java
@@ -41,6 +41,57 @@ public class Transaction {
         return StringUtil.verifyECDSASig(sender, data, signature);
     }
 
+    public boolean canTransaction(){
+        if (!verifySignature()){
+            System.out.println("$ Transaction Signature Failed Verification");
+            return false;
+        }
 
+        for (TransactionInput i : inputs){
+            i.UTXO = GardenOfEden.UTXOs.get(i.transactionOutputID); // demeter got violated bare icl
+        }
+
+        if(getInputsValue() < GardenOfEden.minimumTransaction) {
+			System.out.println("$ Transaction Inputs too small: " + getInputsValue());
+			return false;
+		}
+
+        float remaining = getInputsValue() - amount;
+        transactionID = calculateHash();
+        outputs.add(new TransactionOutput(this.reciever, amount , transactionID)); //send value to recipient
+		outputs.add(new TransactionOutput(this.sender, remaining , transactionID)); //send the left over 'change' back to sender
+
+        for(TransactionOutput o : outputs) {
+			GardenOfEden.UTXOs.put(o.ID, o);
+		}
+		
+		//remove transaction inputs from UTXO lists as spent:
+		for(TransactionInput i : inputs) {
+			if(i.UTXO == null) continue; //if Transaction can't be found skip it 
+			GardenOfEden.UTXOs.remove(i.UTXO.ID);
+		}
+		
+		return true;
+    }
+
+    //returns sum of inputs(UTXOs) values
+	public float getInputsValue() {
+		float total = 0;
+		for(TransactionInput i : inputs) {
+			if(i.UTXO == null) continue; //if Transaction can't be found skip it 
+			total += i.UTXO.value;
+		}
+		return total;
+	}
+
+    //returns sum of outputs:
+	public float getOutputsValue() {
+		float total = 0;
+		for(TransactionOutput o : outputs) {
+			total += o.value;
+		}
+		return total;
+	}
+	
 
 }
--- src/main/java/garden/Wallet.java
+++ src/main/java/garden/Wallet.java
@@ -5,11 +5,18 @@ import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.security.SecureRandom;
 import java.security.spec.ECGenParameterSpec;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import javafx.scene.chart.LineChart.SortingPolicy;
 
 
 public class Wallet {
     public PrivateKey privateKey;
     public PublicKey publicKey;
+	public ArrayList<Transaction> transactionHistory = new ArrayList<Transaction>();
+	public HashMap<String,TransactionOutput> UTXOs = new HashMap<String,TransactionOutput>(); //only UTXOs owned by this wallet.
 
 	// wallet balance = sum of UTXOs
     public Wallet() {
@@ -31,6 +38,55 @@ public class Wallet {
 		} catch(Exception e) {
 			throw new RuntimeException(e);
 		}
-
     }
+
+	// summation of all utxos owed to me = my balance
+	public float getBalance(){
+		float total = 0;
+
+		for (Map.Entry<String, TransactionOutput> item: GardenOfEden.UTXOs.entrySet()){
+			TransactionOutput UTXO = item.getValue();
+
+			if (UTXO.isMine(publicKey)){ // if output belongs to me
+				UTXOs.put(UTXO.id,UTXO); // then add it to list of unspent transactions.
+            	total += UTXO.value ; 
+            }
+		}
+		return total;
+	}
+
+	// send funds if the amount is less than or equal to balance. 
+	public Transaction sendFunds(publicKey reciepient, float amount){
+
+		if (getBalance() < amount){
+			System.out.println("$ Insufficient funds in Wallet ...  Transaction Discarded.");
+			return null;
+		}
+
+		ArrayList<TransactionInput> inputs = new ArrayList<TransactionInput>();
+		float total = 0;
+
+		// create a valid input chain by appending as many needed inputs as the amount required. break when total greater than amount
+		for (Map.Entry<String, TransactionOutput> item: GardenOfEden.UTXOs.entrySet()){
+			TransactionOutput UTXO = item.getValue();
+            total += UTXO.value ; 
+            inputs.add(TransactionInput(UTXO.ID));
+			if (total>amount) break;
+		}
+
+		Transaction newTransaction = new Transaction(publicKey, publicKey, amount, inputs);
+		newTransaction.generateSignature(privateKey);
+
+		// inputs are now no longer valid UTXOs therefore need to remove from memory
+		for (TransactionInput input : inputs){
+			UTXOs.remove(input.transactionOutputID);
+		}
+
+		// maintain prior transactions from this wallet
+		transactionHistory.add(newTransaction);
+		return newTransaction;
+
+	}
+
+
 }
